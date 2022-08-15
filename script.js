b7dbbdaf60a34ae67dd39743e0a5d0ef13cbb349
Hayat Salawdeh <35167530+hayatzsala@users.noreply.github.com>
2022-08-15 00:33:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const sendGift = document.getElementById('send-gift');
+const giftList = document.getElementById('gift-list');
+const data = document.getElementById('inputGift');
+const donateResult = document.getElementById('donate-result');
+const body = document.querySelector('body');
+const donateButtons = Array.prototype.slice.call(document.getElementsByClassName('donate'));
+
+sendGift.addEventListener('click', () => {
+  const para = document.createElement("p");
+  const textNode = document.createTextNode(data.value);
+  para.appendChild(textNode);
+  giftList.appendChild(para);
+});
+
+
+donateButtons.forEach(function(button) {
+  button.addEventListener('click', () => {
+    console.log(button.value);
+    donateResult.innerText = `TOTAL OF ${button.value} HAS BEEN DONATED TO THE BAND... THANK YOU!`
+  }
+  )
+});
+window.addEventListener('load', () => {
+  myFunction();
+})
+function myFunction() {
+  let person = prompt("Please enter your name:");
+  if (person == null || person == "") {
+    text = "Everyone";
+  } else {
+    text = person;
+  }
+  alert(`WELCOME ${text}!`);
+}
